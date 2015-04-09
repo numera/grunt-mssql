9f49d68b6c9233ea570b47e5b8f3faf4f3a5db86
Marco Ensing <ensing@gmail.com>
2015-04-08 20:20:24 -0700

    allow to pass an array of strings for a query to make it a bit more readable

--- tasks/mssql.js
+++ tasks/mssql.js
@@ -2,37 +2,43 @@
 
 module.exports = function(grunt) {
   grunt.registerMultiTask('mssql', 'query to be excuted against a ms sql server', function() {
-      // Merge task-specific and/or target-specific options with these defaults.
-      var options = this.options({
-        server: '',
-        userName: '',
-        password: ''
-      });
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      server: '',
+      userName: '',
+      password: ''
+    });
 
-      var Connection = require('tedious').Connection;
-      var Request = require('tedious').Request;
+    var Connection = require('tedious').Connection;
+    var Request = require('tedious').Request;
 
-      var connection = new Connection({
-          server: options.server,
-          userName: options.userName,
-          password: options.password
-      });
+    var connection = new Connection({
+      server: options.server,
+      userName: options.userName,
+      password: options.password
+    });
 
-      var query = this.data.query;
-      var done = this.async();
-      connection.on('connect', function() {
-          var request = new Request(query, function(err, rowCount) {
-              connection.close();
-              if (err) {
-                grunt.log.error(err);
-                done(false);
-              } else{
-                grunt.log.ok("Query excuted: " + query);
-                done();
-              }
-          });
-          connection.execSql(request);
+    var query = this.data.query;
+    if (this.data.query.constructor === Array) {
+      query = this.data.query.join(' ');
+    } else {
+      query = this.data.query;
+    }
+
+    var done = this.async();
+    connection.on('connect', function() {
+      var request = new Request(query, function(err, rowCount) {
+        connection.close();
+        if (err) {
+          grunt.log.error(err);
+          done(false);
+        } else {
+          grunt.log.ok("Query excuted: " + query);
+          done();
+        }
       });
+      connection.execSql(request);
+    });
   });
 
 };
